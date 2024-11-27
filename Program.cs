63d7c9f4a4b8e914d8a7d94aedd28a5218d29e7f
JhonOsorio20 <jhon.osorio.7e5@itb.cat>
2024-11-27 12:58:56 +0100

    Refactorizando 2

--- Program.cs
+++ Program.cs
@@ -3,29 +3,23 @@ namespace RefactorFork
 {
     public class Program
     {
+        public const string bigOfArray = "Escull introduir la quantitat de nombres entre 15 i 100: ";
+        public const string ForEachNumber = "Introdueix el seguent número, {0}: ";
+        public const string LineBreaker = "\n";
+        public const string Product = "El producte de cada un dels números es: ";
         public static void Main()
         {
             //Constantes
-            const string TextOne = "Escull introduir la quantitat de nombres entre 15 i 100: ";
-            const string ForEachNumber = "Introdueix el seguent número, {0}: ";
-            const string LineBreaker = "\n";
-            const string Product = "El producte de cada un dels números es: ";
+            
 
             int numInputs;
-            Console.WriteLine(TextOne);
+            Console.WriteLine(bigOfArray);
 
             numInputs = Convert.ToInt32(Console.ReadLine());
-            Console.Write(numInputs);         
+            Console.Write(numInputs);
 
             int[] numsToCatch = new int[numInputs];
 
-            // recompte de numeros introduits per l'usuari
-            for (int i = 0; i < numInputs; i++)
-            {
-                string putTheNums = string.Format(ForEachNumber, i + 1);
-                Console.Write(putTheNums + LineBreaker);
-                numsToCatch[i] = Convert.ToInt32(Console.ReadLine());
-            }
           
             int productAllNumbers = 0;
             for (int i = 0; i < numInputs; i++)
@@ -34,6 +28,18 @@ namespace RefactorFork
             }
 
             Console.WriteLine(Product + productAllNumbers);
+        }
+
+        public static void FullmentArray(int[] numArray)
+        {
+
+            // recompte de numeros introduits per l'usuari
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                string putTheNums = string.Format(ForEachNumber, i + 1);
+                Console.Write(putTheNums + LineBreaker);
+                numsToCatch[i] = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
